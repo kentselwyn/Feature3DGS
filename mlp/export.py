e8ca2e2ec3e51ecb1fc44ca909919f04b5c13266
kokifhhh123 <koki20001019@gmail.com>
2025-02-07 13:16:50 +0800

    make more comprehensive export script

--- mlp/export.py
+++ mlp/export.py
@@ -10,24 +10,12 @@ from .export_predictions import export_predictions
 from encoders.superpoint.superpoint import SuperPoint
 
 
-path = Path(f"/home/koki/code/cc/feature_3dgs_2/data/vis_loc/gsplatloc/Cambridge/Cambridge_StMarysChurch/train/rgb")
-scene_name = path.parts[-4]
-data_name = path.parts[-3]
-
 random.seed(0)
-
-    
 class MLP_sp_data_scannet(Dataset):
     def __init__(self, conf) -> None:
-        # glob = ["*.jpg", "*.png"]
-        # g = glob[0]
         conf = OmegaConf.create(conf)
         images = list(conf.path.glob("**/" + "*.png"))
-        # images = [path for path in images_ if 'all_images' not in path.parts]
         random.shuffle(images)
-        
-        # s_sample = random.sample(s_images, 50000)
-
         self.images = images
         self.preprocessor = ImagePreprocessor(conf.preprocessing)
 
@@ -36,7 +24,6 @@ class MLP_sp_data_scannet(Dataset):
         img = load_image(path=path)
         data = self.preprocessor(img)
         data['name'] = str(path)
-
         return data
     
     def __len__(self):
@@ -44,11 +31,9 @@ class MLP_sp_data_scannet(Dataset):
     
 
 n_kpts = 1024
-resize = 1024
-configs = {
-    "sp": {
-        "name": f"r{resize}_SP-k{n_kpts}-nms4-{data_name}",
-        "keys": ["keypoints", "descriptors", ],
+resize = 640
+extractor_configs = {
+    "SP": {
         "gray": True,
         "conf": {
             "name": "superpoint",
@@ -58,66 +43,51 @@ configs = {
             "force_num_keypoints": True,
         },
     },
-    "sift": {
-        "name": f"r{resize}_SIFT-k{n_kpts}",
-        "keys": ["keypoints", "descriptors" ],
-        "gray": True,
-        "conf": {
-            "name": "sift",
-            "max_num_keypoints": n_kpts,
-            "options": {
-                "peak_threshold": 0.001,
-            },
-            "peak_threshold": 0.001,
-            "device": "cpu",
-            "force_num_keypoints": True,
-        },
-    },
 }
 
 
-
-def run_export(args, feature_file):
+def run_export(args, data_path, feature_file):
     conf = {
         "data":{
-            "path": path,
+            "path": data_path,
             "preprocessing": {
                 'resize': resize,
                 'side': 'long',
                 'square_pad': False,
             },
         },
-        "model": configs[args.method],
+        "model": extractor_configs[args.method],
     }
     conf = OmegaConf.create(conf)
-    keys = configs[args.method]["keys"]
+    keys = extractor_configs[args.method]["keys"]
     dataset = MLP_sp_data_scannet(conf.data)
-
     loader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=collate, num_workers=4)
     device = "cuda" if torch.cuda.is_available() else "cpu"
-
     model = SuperPoint(conf.model.conf).eval().to(device)
-    # breakpoint()
-
     export_predictions(loader, model, feature_file, as_half=True, keys=keys)
-
-
-
-data_path = f"/home/koki/code/cc/feature_3dgs_2/data/vis_loc/gsplatloc/{scene_name}/{data_name}/desc_data"
 
 
 # python -m mlp.export --method sp
 if __name__=="__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--method", type=str, default="sp")
+    parser.add_argument("--method", type=str, default="SP")
+    parser.add_argument("--data_name", type=str)
+    parser.add_argument("--scene_name", type=str)
+    parser.add_argument("--dense", action='store_true')
     args = parser.parse_args()
-    export_name = configs[args.method]["name"]
-    feature_file = Path(
-            data_path, export_name + ".h5"
-    )
-    run_export(args, feature_file=feature_file)
+    name = f"{args.data_name}_{args.scene_name}"
 
+    if args.dense:
+        export_name = f"r{resize}_{args.method}-k{n_kpts}-nms4-{name}-dense"
+        keys = ["keypoints", "descriptors", "dense_descriptors"]
+    else:
+        export_name = f"r{resize}_{args.method}-k{n_kpts}-nms4-{name}"
+        keys = ["dense_descriptors"]
+    
+    data_name = args.data_name
+    scene_name = args.scene_name
+    data_path = f"/home/koki/code/cc/feature_3dgs_2/data/vis_loc/gsplatloc/{data_name}/{scene_name}/train/rgb"
+    out_path = f"/home/koki/code/cc/feature_3dgs_2/data/vis_loc/gsplatloc/{data_name}/{scene_name}/desc_data"
 
-
-
-
+    feature_file = Path(out_path, export_name + ".h5")
+    run_export(args, data_path=data_path, feature_file=feature_file)
