48f59ff70cab0fff165d59855975acc117780d13
kokifhhh123 <koki20001019@gmail.com>
2025-02-19 02:51:14 +0800

    change the oredr to unified shell, add another one

--- zenith_scripts/DatasetBuild_Train_Loc2.sh
+++ zenith_scripts/DatasetBuild_Train_Loc2.sh
@@ -5,63 +5,75 @@
     # Cambridge
     data_name="Cambridge"
     ###############################################
-    # pgt_7scenes_pumpkin, pgt_7scenes_stairs, pgt_7scenes_chess, pgt_7scenes_fire, pgt_7scenes_heads, pgt_7scenes_office, pgt_7scenes_redkitchen
+    # pgt_7scenes_chess, pgt_7scenes_fire, pgt_7scenes_heads, pgt_7scenes_office, pgt_7scenes_pumpkin, pgt7scenes_redkitchen, pgt_7scenes_stairs
     # Cambridge_KingsCollege, Cambridge_OldHospital, Cambridge_ShopFacade, Cambridge_StMarysChurch
-    scene_name="Cambridge_ShopFacade" 
+    scene_name="Cambridge_KingsCollege" 
     ###############################################
     # all
-    # pgt_7scenes_pumpkin, augment_pgt_7scenes_stairs, pgt_7scenes_chess, pgt_7scenes_fire, pgt_7scenes_heads, pgt_7scenes_office, pgt_7scenes_redkitche
+    # pgt_7scenes_chess, pgt_7scenes_fire, pgt_7scenes_heads, pgt_7scenes_office, pgt_7scenes_pumpkin, pgt_7scenes_redkitchen, pgt_7scenes_stairs
     # Cambridge
     # Cambridge_KingsCollege, Cambridge_OldHospital, Cambridge_ShopFacade, Cambridge_StMarysChurch
-    mlp_method="Cambridge"
+    mlp_method="Cambridge_KingsCollege"
     ###### dataset build #########################################
     resize_num=1
     th=0.01
     mlp_dim=16
-    max_num_keypoints=512
+    max_num_keypoints=1024
+    # data_ImgName="imrate:${resize_num}_th:${th}_kptnum:${max_num_keypoints}"
+    data_ImgName="None"
+    # feature name
+    # feat_option="small"
+    feat_name="${mlp_method}_imrate:${resize_num}_th:${th}_"
+    feat_name+="mlpdim:${mlp_dim}_kptnum:${max_num_keypoints}_rgb"
+}
+# ( bash zenith_scripts/DatasetBuild_Train_Loc2.sh )
+{
     ###### gaussian training #########################################
-    score_loss="L2" # L2, weighted, L1
+    score_loss="weighted" # L2, weighted, L1
     score_scale=1
-    render_num=20
-    if_render=1
+    feature_opa=0
+    # train_ImgName="imrate:${resize_num}_th:${th}_kptnum:${max_num_keypoints}"
+    train_ImgName="rgb"
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Warninggggggggggggggggggggggggggggggggggggggggg
     traing_name_option="UseTrueRender"
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Warninggggggggggggggggggggggggggggggggggggggggg
+    # output name
+    out_name="${mlp_method}_imrate:${resize_num}_th:${th}_"
+    out_name+="mlpdim:${mlp_dim}_kptnum:${max_num_keypoints}_"
+    out_name+="Score${score_loss}_ScoreScale${score_scale}_${train_ImgName}_${traing_name_option}"
+    if (( feature_opa )); then
+        out_name+="_FeatureOpa"
+    fi
+    # render
+    render_num=20
+    if_render=1
 }
 
 
 # ( bash zenith_scripts/DatasetBuild_Train_Loc2.sh )
 ########################################################################
-build_data=0
-start_train=0
+build_data=1
+start_train=1
 start_loc=1
 ########################################################################
 
 
 ##### localization parameters ######################################################################## 
 {
+    rival=0
     save_match=0
+    feat_from_img=0  # use score from gaussian, feature from image
+    ###########################
+    depth_render=1
+    ###########################
     test_iteration=30000
     sp_th=0.01
     lg_th=0.01
-    kpt_hist=0.9
+    kpt_hist=0.98
     ransac_iters=20000
     kernel_size=15
     stop_kpt_num=50
     pnp_option="pycolmap" #iters, epnp, pycolmap
-    depth_render=0
-    rival=0
-}
-##### determine output name ##################################################################
-{
-    # feature name
-    feat_name="${mlp_method}_imrate:${resize_num}_th:${th}_"
-    feat_name+="mlpdim:${mlp_dim}_kptnum:${max_num_keypoints}_rgb"
-
-    # output name
-    out_name="${mlp_method}_imrate:${resize_num}_th:${th}_"
-    out_name+="mlpdim:${mlp_dim}_kptnum:${max_num_keypoints}_"
-    out_name+="Score${score_loss}_ScoreScale${score_scale}_rgb_${traing_name_option}"
 
     # localization name
     test_name="iteration${test_iteration}_sp${sp_th}_lg${lg_th}_kptth0.01_"
@@ -72,11 +84,14 @@ start_loc=1
         test_name+="DepthRender"
     fi
 
+    if (( feat_from_img )); then
+        test_name+="FeatFromImg"
+    fi
+
     if (( rival )); then
         test_name+="_rival_000"
     fi
 }
-
 
 SOURSE_PATH="/home/koki/code/cc/feature_3dgs_2/data/vis_loc/gsplatloc/$data_name/$scene_name"
 feature_name="features/$feat_name"
@@ -87,14 +102,17 @@ if (( build_data )); then
     s_path_train="/home/koki/code/cc/feature_3dgs_2/data/vis_loc/gsplatloc/${data_name}/${scene_name}/train"
     s_path_test="/home/koki/code/cc/feature_3dgs_2/data/vis_loc/gsplatloc/${data_name}/${scene_name}/test"
     python dataset_build.py --source_path "$s_path_train" --method "$mlp_method" --mlp_dim $mlp_dim  --images rgb \
+                            --output_images "$data_ImgName" \
                             --resize_num $resize_num --th $th --max_num_keypoints $max_num_keypoints --name $feat_name
     python dataset_build.py --source_path "$s_path_test" --method "$mlp_method" --mlp_dim $mlp_dim --images rgb \
+                            --output_images "$data_ImgName" \
                             --resize_num $resize_num --th $th --max_num_keypoints $max_num_keypoints --name $feat_name
 fi
 
 ######################################################################## train ########################################################################
 if (( start_train )); then
-    python train.py -s "$SOURSE_PATH" -m "$OUT_PATH" -f "$feature_name" --iterations 30000 \
+    export FEATURE_OPA=$feature_opa
+    python train.py -s "$SOURSE_PATH" -i "$train_ImgName" -m "$OUT_PATH" -f "$feature_name" --iterations 30000 \
                     --score_loss "$score_loss" --score_scale "$score_scale"
     if (( if_render )); then
         python render.py -m $OUT_PATH --iteration 30000 --skip_train --view_num $render_num
@@ -105,6 +123,7 @@ fi
 if (( start_loc )); then
     export MLP_DIM=$mlp_dim
     export DEPTH_RENSER=$depth_render
+    export FEATURE_OPA=$feature_opa
     if [[ "$data_name" == "7_scenes" ]]; then
         ace_ckpt="/home/koki/code/cc/feature_3dgs_2/data/ace_models/7Scenes_pgt/$scene_name.pt"
     elif [[ "$data_name" == "Cambridge" ]]; then
@@ -119,12 +138,12 @@ if (( start_loc )); then
                                     --mlp_dim $mlp_dim --mlp_method $mlp_method --lg_th $lg_th \
                                     --kernel_size $kernel_size --sp_th $sp_th --ransac_iters $ransac_iters \
                                     --stop_kpt_num $stop_kpt_num --pnp $pnp_option --kpt_hist $kpt_hist \
-                                    --test_name "$test_name" --rival "$rival"
+                                    --test_name "$test_name" --rival "$rival" --feat_from_img "$feat_from_img"
     else
         python loc_inference_ace.py -m $OUT_PATH --ace_ckpt $ace_ckpt --iteration $test_iteration \
                                     --mlp_dim $mlp_dim --mlp_method $mlp_method --lg_th $lg_th \
                                     --kernel_size $kernel_size --sp_th $sp_th --ransac_iters $ransac_iters \
                                     --stop_kpt_num $stop_kpt_num --pnp $pnp_option --kpt_hist $kpt_hist \
-                                    --test_name "$test_name" --rival "$rival"
+                                    --test_name "$test_name" --rival "$rival" --feat_from_img "$feat_from_img"
     fi
 fi
