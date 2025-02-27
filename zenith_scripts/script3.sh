f53c3ebc784c86cd380f3b971f9b1010f46274b8
kokifhhh123 <koki20001019@gmail.com>
2025-02-27 23:22:12 +0800

    add some utility scripts

---
+++ zenith_scripts/script3.sh
@@ -0,0 +1,4 @@
+( bash zenith_scripts/DatasetBuild_Train_Loc2.sh Cambridge_OldHospital )
+( bash zenith_scripts/DatasetBuild_Train_Loc2.sh Cambridge_ShopFacade )
+( bash zenith_scripts/DatasetBuild_Train_Loc2.sh Cambridge_StMarysChurch )
+# bash zenith_scripts/script3.sh
